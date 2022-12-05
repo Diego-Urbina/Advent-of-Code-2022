6542d90c89f0bbfe3a570f6f666c1b1f91512914
diego.perezurbina <diego.perez@wallbox.com>
2022-12-05 08:19:50 +0100

    add utils::string

---
+++ src/utils/string.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace utils
+{
+    namespace string
+    {
+        static std::vector<std::string> split(const std::string &string, char delimiter)
+        {
+            std::vector<std::string> list;
+
+            size_t last = 0;
+            size_t next = 0;
+            while ((next = string.find(delimiter, last)) != std::string::npos) {
+                list.push_back(string.substr(last, next - last));
+                last = next + 1;
+            }
+            if (!string.empty()) {
+                list.push_back(string.substr(last));
+            }
+
+            return list;
+        }
+    }
+}
